140b63dc715481127fd48fac37fb740e7e81c431
Paul LOPEZ <p.lopez@alienor.net>
2024-03-15 23:20:17 +0100

    commande de génération des lettres via un document csv

--- src/Command/GenerateLetterCommand.php
+++ src/Command/GenerateLetterCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Command;
 
+use Gotenberg\Gotenberg;
+use Gotenberg\Stream;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -9,6 +11,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Twig\Environment;
 
 #[AsCommand(
     name: 'app:generate-letter',
@@ -16,33 +20,57 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class GenerateLetterCommand extends Command
 {
-    public function __construct()
+    private Environment $environment;
+    private ParameterBagInterface $parameterBag;
+
+    public function __construct(Environment $environment, ParameterBagInterface $parameterBag)
     {
         parent::__construct();
+        $this->environment = $environment;
+        $this->parameterBag = $parameterBag;
     }
 
     protected function configure(): void
     {
-//        $this
-//            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+        $this
+            ->addArgument('fichier', InputArgument::REQUIRED, 'Fichier csv de Data')
 //            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-//        ;
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $dates = [
+            'dateExtract' => new \DateTime('2024-03-01'),
+            'dateRetourMax' => new \DateTime('2024-04-01'),
+        ];
+
+        $apiUrl = 'http://localhost:3000/';
+
+        $csv = $input->getArgument('fichier');
+        if (($handle = fopen($csv, "r")) !== FALSE) {
+            $header = fgetcsv($handle, 1000, ",");
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $dataCombine = array_combine($header, $data);
+                Gotenberg::save(
+                    Gotenberg::chromium($apiUrl)
+                        ->pdf()
+                        ->outputFilename($dataCombine['cityName'])
+                        ->html(
+                            Stream::string('content', $this->environment->render('letter/index.html.twig', [
+                                'data' => array_merge(
+                                    $dates,
+                                    $dataCombine
+                                )
+                            ])),
+                        )
+                    ,
+                    $this->parameterBag->get('kernel.project_dir').'generate-letter/'
+                );
+            }
         }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
         return Command::SUCCESS;
     }
